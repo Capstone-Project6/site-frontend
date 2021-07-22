0f7998eff0c5cfd74daaceb1b6ea04c175868b01
Sharon Sanchez <sharon@Sharons-iMac.local>
2021-07-22 15:23:59 -0700

    added to form

--- event-project-ui/src/components/createEvent/createEvent.js
+++ event-project-ui/src/components/createEvent/createEvent.js
@@ -3,29 +3,83 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import DateFnsUtils from '@date-io/date-fns';
 import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import "./createEvent.css"
+
+//Add City and State
+
 import {
   MuiPickersUtilsProvider,
   KeyboardTimePicker,
   KeyboardDatePicker,
 } from '@material-ui/pickers';
 
-export default function CreateEvent() {
-  // The first commit of Material-UI
-  const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
+// This is for the category dropdown
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+  }));
 
+ 
+
+export default function CreateEvent() {
+  // This is for the time material UI
+  const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
   const handleDateChange = (date) => {
     setSelectedDate(date);
   };
+
+  // This is for the category Dropdown
+  const classes = useStyles();
+  const [event_category, setAge] = React.useState('');
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+  
   
   return (
+    <div className="createPage">
+    <h1 className="Title">Let's Create an Event!</h1>
+    <h2 className="basicInfo">Basic Info</h2>
     
     <MuiPickersUtilsProvider utils={DateFnsUtils}>
       <Grid container justifyContent="space-around">
-      <h1 className="Title">Let's Create an Event!</h1>
-      <h2 className="basicInfo">Basic Info</h2>
+      
+      
+      <h3 className="eventName">Event Name</h3>
       <h3 className="eventName">Event Organizer</h3>
       <h3 className="eventDescription">Event Description</h3>
-    
+
+        {/* Event Category dropdown from Material UI*/}
+      <FormControl variant="outlined" className={classes.formControl}>
+        <InputLabel id="demo-simple-select-outlined-label">Category</InputLabel>
+        <Select
+          labelId="demo-simple-select-outlined-label"
+          id="demo-simple-select-outlined"
+          value={event_category}
+          onChange={handleChange}
+          label="Event Category"
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={10}>Music</MenuItem>
+          <MenuItem value={20}>Sports</MenuItem>
+          <MenuItem value={30}>Food</MenuItem>
+          <MenuItem value={30}>Charity</MenuItem>
+        </Select>
+      </FormControl>
+
+   {/* Event Calendar and time from Material UI*/} 
         <KeyboardDatePicker
           className="startDate"
           disableToolbar
@@ -53,9 +107,8 @@ export default function CreateEvent() {
           }}
         />
         <KeyboardTimePicker
-          className="startTime"
           margin="normal"
-          id="start-time"
+          id="time-picker"
           label="Start Time"
           value={selectedDate}
           onChange={handleDateChange}
@@ -63,10 +116,10 @@ export default function CreateEvent() {
             'aria-label': 'change time',
           }}
         />
+
         <KeyboardTimePicker
-          className="endTime"
           margin="normal"
-          id="end-time"
+          id="time-picker"
           label="End Time"
           value={selectedDate}
           onChange={handleDateChange}
@@ -74,8 +127,9 @@ export default function CreateEvent() {
             'aria-label': 'change time',
           }}
         />
+        
       </Grid>
     </MuiPickersUtilsProvider>
-  );
-
+    </div>
+  )
 }
