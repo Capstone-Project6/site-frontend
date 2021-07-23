557dd8a801b6a11e87d45e60e3a8523c83b691a6
Abbey311 <y.abbey2@gmail.com>
2021-07-23 18:41:38 -0400

    submit button appears

--- event-project-ui/src/components/Interests/interests.js
+++ event-project-ui/src/components/Interests/interests.js
@@ -8,8 +8,26 @@ import InterestsCard from './InterestsCard';
 import Charity from '../../Charity.jpg'
 import Food from '../../Food.jpg'
 import "./Interests.css"
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import { blue } from '@material-ui/core/colors';
+import { CenterFocusStrong } from '@material-ui/icons';
+import { flexbox } from '@material-ui/system';
+
+const useStyles = makeStyles({
+    root: {
+      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+      color: 'white',
+      height: 48,
+      padding: '0 30px',
+      marginTop: 30,
+      justifyContent: "center",
+
+    },
+  });
 
 export default function Interests( { user, isFetching, events, error }){
+    const classes = useStyles();
     const [alignment, setAlignment] = useState('left');
 
     const handleAlignment = (event, newAlignment) => {
@@ -24,7 +42,6 @@ export default function Interests( { user, isFetching, events, error }){
         {
             id: 2,
             eventImage: Music,
-
         },
         {
             id: 3,
@@ -33,7 +50,6 @@ export default function Interests( { user, isFetching, events, error }){
         {
             id: 4,
             eventImage: Charity,
-
         },
 
     ];
@@ -51,6 +67,7 @@ export default function Interests( { user, isFetching, events, error }){
                         ))}
                         </div>
                     </>
+                    <Button className={classes.root}>Submit</Button>
 
         </div>
     )
