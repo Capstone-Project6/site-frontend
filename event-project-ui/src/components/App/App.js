9c656b16612eb55d508d1793ced156729e64b616
Abbey Yared <y.abbey2@gmail.com>
2021-08-04 13:36:12 -0400

    filename change

--- event-project-ui/src/components/App/App.js
+++ event-project-ui/src/components/App/App.js
@@ -6,7 +6,7 @@ import Navbar from '../Navbar/Navbar';
 import Home from '../Home/Home';
 import Login from '../Login/Login';
 import Signup from '../SignUp/SignUp';
-import Interests from '../Interests/Interests';
+import Interests from '../Interests/interests';
 import EventgoerProfile from '../EventgoerProfile/EventgoerProfile';
 import Filter from '../Filter/Filter';
 import './App.css';
