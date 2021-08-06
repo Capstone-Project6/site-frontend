fea0a56fb6a7af1a4e16362967df0a1dba0ea44d
Sydney Varner <sgabbyv@gmail.com>
2021-08-06 15:02:05 -0400

    Basic sturcture of event registration page

--- event-project-ui/src/components/EventRegistration/EventRegistration.js
+++ event-project-ui/src/components/EventRegistration/EventRegistration.js
@@ -3,9 +3,13 @@ import { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom"
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
+import { spacing } from '@material-ui/system';
 import { makeStyles } from '@material-ui/core/styles';
 import './EventRegistration.css'
-import { Typography } from '@material-ui/core';
+import { CardContent, Typography } from '@material-ui/core';
 
 export default function EventRegistration({user}){
     // const { id } = useParams()
@@ -39,55 +43,94 @@ export default function EventRegistration({user}){
         eventRegistrationContent : {
             display: "flex",
             flexDirection: "row",
-            justifyContent: "space-around"
+            justifyContent: "space-around",
+            margin: "auto"
+        },
+        eventInfo: {
+            margin: 40,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between"
+        },
+        registrationForm: {
+            background: "#C4C4C4",
+            margin: 40
+        },
+        eventPosterContainer: {
+            margin: "auto",
+
+        },
+        aboutContainer: {
+            background: "#F0F8FF",
+            borderRadius: 10,
+            height: 100
+        },
+        aboutBox: {
+            height: "100%"
         }
     });
     
     const classes = useStyles();
 
     const renderEventContent = () => {
-        if(isLoading){
-            return <h1>Loading...</h1>
-        }
-        if(error){
-            return(
-                <>
-                    <h1> Error</h1>
-                    <p className="error"> {String(error)}</p>
-                </>
-            )
-        }
+        // if(isLoading){
+        //     return <h1>Loading...</h1>
+        // }
+        // if(error){
+        //     return(
+        //         <>
+        //             <h1> Error</h1>
+        //             <p className="error"> {String(error)}</p>
+        //         </>
+        //     )
+        // }
 
         return (
-            <div className="eventRegistration">
-                <h1> RegistrationPage</h1>
-                <Box className={classes.eventRegistrationContent}>
-                    <Grid container className={classes.eventInfo}>
-                        <Grid item>
-                            <Typography> 
-                                Event Poster
-                            </Typography>
-                            <Typography>
-                                Event Details
-                            </Typography>
-                            <Typography>
-                                Event description
-                            </Typography>
+            <>
+                <div className="eventRegistrationContent">
+                    <h1 className="eventRegistrationTitle"> Event Name</h1>
+                    <Box className={classes.eventRegistrationContent}>
+                        <Grid container className={classes.eventInfo}>
+                            <Grid item className={classes.eventPosterContainer}>
+                                <Card className={classes.eventPosterCard}>
+                                    <CardActionArea>
+                                        <CardMedia
+                                            component="img"
+                                            alt="Stock party image"
+                                            image="https://images.pexels.com/photos/1684187/pexels-photo-1684187.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+                                        />
+                                        <CardContent>
+                                            <Typography>
+                                                Details
+                                            </Typography>
+                                        </CardContent>
+                                    </CardActionArea>
+                                </Card>
+                            </Grid>
+                            <Grid item className={classes.aboutContainer}>
+                                <Box className={classes.aboutBox}>
+                                    <Typography>
+                                        About
+                                    </Typography>
+                                </Box>
+                            </Grid>
                         </Grid>
-                    </Grid>
-                    <Grid container className={classes.registrationForm}>
-                        <Grid item>
-                            <Typography> Registration Form</Typography>
+                        <Grid container className={classes.registrationForm}>
+                            <Grid item>
+                                <Typography> Registration Form</Typography>
+                            </Grid>
                         </Grid>
-                    </Grid>
-                </Box>
-            </div>
+                    </Box>
+                </div>
+            </>
         )
     }
 
     return (
         <div className="eventRegistrationPage">
-            {renderEventContent}
+            <div className="eventRegistrationContent">
+                {renderEventContent()}
+            </div>
         </div>
     )
 }
